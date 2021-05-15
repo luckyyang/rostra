58c61e6582a24d7d08bc5d07913695c667c8bb7d
HA <>
2021-05-15 13:12:26 +0800

    add pages

--- packages/react-app/src/views/index.js
+++ packages/react-app/src/views/index.js
@@ -1,3 +1,10 @@
 export { default as ExampleUI } from "./ExampleUI";
 export { default as Hints } from "./Hints";
 export { default as Subgraph } from "./Subgraph";
+export { default as CourseDonationList } from "./CourseDonationList";
+export { default as SchoolList } from "./SchoolList";
+export { default as CreateCourse } from "./CreateCourse";
+export { default as CreateDonationEvent } from "./CreateDonationEvent";
+export { default as SchoolDetail } from "./SchoolDetail";
+export { default as CreateSchool } from "./CreateSchool";
+export { default as SchoolDonationList } from "./SchoolDonationList";
